29eda97d5bf8c17d4ac19ad31a3f4d184823ffc4
Eduardo <eduardojhoanes@gmail.com>
2021-01-02 23:46:18 -0300

    newfiles2

--- conn.py
+++ conn.py
@@ -32,6 +32,27 @@ def verificar():
     
     return render_template('index.html',classe=str(classe), probab=(probab))
 
+
+def import_module(name, package=None):
+    """Import a module.
+    The 'package' argument is required when performing a relative import. It
+    specifies the package to use as the anchor point from which to resolve the
+    relative import to an absolute import.
+
+    """
+        level = 0
+    if name.startswith('.'):
+        if not package:
+            msg = ("the 'package' argument is required to perform a relative "
+                   "import for {!r}")
+                        raise TypeError(msg.format(name))
+        for character in name:
+            if character != '.':
+                break
+            level += 1
+    return _bootstrap._gcd_import(name[level:], package, level)
+
+
 if __name__ == "__main__":
   
     app.run()
