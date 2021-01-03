8597fe6f375137d022ebb8e8b50905a6582a8ab9
Eduardo <eduardojhoanes@gmail.com>
2021-01-02 23:25:33 -0300

    newfiles

--- conn.py
+++ conn.py
@@ -4,16 +4,15 @@ from flask import Flask, request, render_template, make_response
 import joblib
 
 app = Flask(__name__)
-model = joblib.load('Modelo/model.pkl')
+model = joblib.load('modelo/model.pkl')
 
 
-@app.route("/")
+@app.route('/')
 
-def about():
-    return render_template('ml.html')
+def display_gui():
+    return render_template('index.html')
 
-    
-@app.route("/verificar/", methods=['POST'])
+@app.route('/verificar', methods=['POST'])
 def verificar():
     Pregnancies = request.form['Pregnancies']
     Glucose = request.form['Glucose']
@@ -25,29 +24,15 @@ def verificar():
     Age = request.form['Age']
     teste = np.array([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
     
-    print(":::::: Dados de Teste :::::::")
-    print("Pregnancies: {}".format(Pregnancies))
-    print("Glucose: {}".format(Glucose))
-    print("BloodPressure: {}".format(BloodPressure))
-    print("SkinThickness: {}".format(SkinThickness))
-    print("Insulin: {}".format(Insulin))
-    print("BMI: {}".format(BMI))
-    print("DiabetesPedigreeFunction: {}".format(DiabetesPedigreeFunction))
-    print("Age: {}".format(Age))
-    print("\n")
-    
-        
+          
     classe = model.predict(teste)[0]
     prob = model.predict_proba(teste)[0]
     probab = round(np.max(prob)*100,2)
     
-    print("Classe Predita: {}".format(str(classe)))
-    print("A probalidade é de: {}".format(probab))
-
-    return render_template('ml.html',classe=str(classe), probab=(probab))
+    
+    return render_template('index.html',classe=str(classe), probab=(probab))
 
 if __name__ == "__main__":
   
-    port = os.environ.get('port',5000)
-    app.run(host='0.0.0.0',port=port)
+    app.run()
     
