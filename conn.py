d57aaecf3e93f43e8599b83b702aba28095a0854
Eduardo <eduardojhoanes@gmail.com>
2021-01-02 23:52:47 -0300

    newfile4

--- conn.py
+++ conn.py
@@ -4,7 +4,7 @@ from flask import Flask, request, render_template, make_response
 import joblib
 
 app = Flask(__name__)
-model = joblib.load('modelo/model.pkl')
+model = joblib.load('Modelo/model.pkl')
 
 
 @app.route('/')
@@ -34,20 +34,23 @@ def verificar():
 
 
 def import_module(name, package=None):
+
     """Import a module.
     The 'package' argument is required when performing a relative import. It
     specifies the package to use as the anchor point from which to resolve the
     relative import to an absolute import.
 
     """
-        level = 0
+    level = 0
     if name.startswith('.'):
         if not package:
+
             msg = ("the 'package' argument is required to perform a relative "
                    "import for {!r}")
-                        raise TypeError(msg.format(name))
+            raise TypeError(msg.format(name))
         for character in name:
             if character != '.':
+
                 break
             level += 1
     return _bootstrap._gcd_import(name[level:], package, level)
